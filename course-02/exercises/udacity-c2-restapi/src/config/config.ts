db9e7f6a100ba86d0595256390644dca9ac94ba7
David <learnitmyway@users.noreply.github.com>
2020-03-28 18:18:22 +0100

    (c2-restapi) Add region, profile and S3 to config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -4,16 +4,16 @@ export const config = {
     password: process.env.CONFIG_DEV_PASSWORD,
     database: process.env.CONFIG_DEV_DATABASE,
     host: process.env.CONFIG_DEV_HOST,
-    dialect: "postgres",
-    aws_region: "",
-    aws_profile: "",
-    aws_media_bucket: ""
+    dialect: 'postgres',
+    aws_region: process.env.CONFIG_DEV_AWS_REGION,
+    aws_profile: process.env.CONFIG_DEV_AWS_PROFILE,
+    aws_media_bucket: process.env.CONFIG_DEV_AWS_MEDIA_BUCKET
   },
   prod: {
-    username: "",
-    password: "",
-    database: "udagram_prod",
-    host: "",
-    dialect: "postgres"
+    username: '',
+    password: '',
+    database: 'udagram_prod',
+    host: '',
+    dialect: 'postgres'
   }
-};
+}
