59f3c624d9e44a9543725732dd950efb9b180c91
David <learnitmyway@users.noreply.github.com>
2020-03-28 17:36:46 +0100

    (c2-restapi) Add db config vars

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.CONFIG_DEV_USERNAME,
+    password: process.env.CONFIG_DEV_PASSWORD,
+    database: process.env.CONFIG_DEV_DATABASE,
+    host: process.env.CONFIG_DEV_HOST,
+    dialect: "postgres",
+    aws_region: "",
+    aws_profile: "",
+    aws_media_bucket: ""
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres"
   }
-}
+};
