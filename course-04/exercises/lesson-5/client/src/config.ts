62f6fe08e86f4c25bd8410a5077233eb6c2a6a9a
David <learnitmyway@users.noreply.github.com>
2020-04-10 16:42:43 +0200

    course-04/exercises/lesson-5/client: Update config

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,4 +1,4 @@
-export const apiEndpoint = process.env.endpoint
+export const apiEndpoint = `https://${process.env.apiKey}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: process.env.domain,
