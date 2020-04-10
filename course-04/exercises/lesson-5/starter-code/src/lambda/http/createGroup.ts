64adb931270c93fdfafb15c98e0e213b43916f40
David <learnitmyway@users.noreply.github.com>
2020-04-10 16:28:00 +0200

    course-04/exercises/lesson-5/starter-code: Add user id to new group

--- course-04/exercises/lesson-5/starter-code/src/lambda/http/createGroup.ts
+++ course-04/exercises/lesson-5/starter-code/src/lambda/http/createGroup.ts
@@ -1,35 +1,51 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
+import { getUserId } from '../../auth/utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
   const itemId = uuid.v4()
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const userId = getUserId(jwtToken)
 
   const parsedBody = JSON.parse(event.body)
 
   const newItem = {
     id: itemId,
-    ...parsedBody
+    userId,
+    ...parsedBody,
   }
 
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+  await docClient
+    .put({
+      TableName: groupsTable,
+      Item: newItem,
+    })
+    .promise()
 
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
+      'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify({
-      newItem
-    })
+      newItem,
+    }),
   }
 }
